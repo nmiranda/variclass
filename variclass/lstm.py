7fd3231f5b3f0ae43d8400c380ed379c21e8af9c
nmiranda <nmiranda@dcc.uchile.cl>
2017-06-19 17:03:22 -0400

    changes to input and output

--- variclass/lstm.py
+++ variclass/lstm.py
@@ -40,13 +40,14 @@ def main():
 			max_jd = this_jd.shape[0]
 
 		type_list.append(this_header['TYPE_SPEC'])
-		jd_list.append(this_jd[:-1])
+		jd_list.append(this_jd[1:])
 		q_list.append(this_q[:-1])
 		jd_delta_list.append(this_jd[1:] - this_jd[:-1])
 		q_pred_list.append(this_q[1:])
+	max_jd = max_jd-1
 
-	data_X = np.full((len(jd_list),max_jd,3), 0.)
-	for i in range(len(jd_list)):
+	data_X = np.full((len(jd_list), max_jd, 3), 0.)
+	for i in xrange(len(jd_list)):
 		data_X[i,:jd_list[i].shape[0],0] = jd_list[i]
 		data_X[i,:jd_delta_list[i].shape[0],1] = jd_delta_list[i]
 		data_X[i,:q_list[i].shape[0],1] = q_list[i]
@@ -56,12 +57,16 @@ def main():
 
 	label_encoder = LabelEncoder().fit(type_list)
 	type_list = label_encoder.transform(type_list)
-	data_Y = np.full((len(jd_list),len(label_encoder.classes_)), 0.)
+	data_Y = np.full((len(jd_list)*2, max_jd, len(label_encoder.classes_)+1), 0.)
+
 	for i, value in enumerate(type_list):
-		data_Y[i][value] = 1
+		data_Y[i,max_jd-1,value] = 1
+		data_Y[i,:len(jd_list[i]),len(label_encoder.classes_)] = jd_list[i]
+
+	import ipdb;ipdb.set_trace()
 
 	model = Sequential()
-	model.add(LSTM(64, input_dim=3))
+	model.add(LSTM(64, input_dim=4, return_sequences=True))
 	model.add(Dense(len(label_encoder.classes_)))
 	model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 	model.fit(data_X, data_Y, epochs=5, batch_size=1, verbose=2)
