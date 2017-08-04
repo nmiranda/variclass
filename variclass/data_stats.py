bd1da8109c37062806b5d236df96166e161d98e1
nmiranda <nmiranda@dcc.uchile.cl>
2017-08-03 20:17:07 -0400

    major changes

--- variclass/data_stats.py
+++ variclass/data_stats.py
@@ -4,6 +4,7 @@ import glob
 import os
 import pyfits
 from collections import Counter
+import matplotlib.pyplot as plt
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -11,18 +12,30 @@ def main():
 	args = parser.parse_args()
 
 	type_list = list()
+	num_obs_list = list()
 	fits_files = glob.glob(os.path.join(args.dir, '*.fits'))
 
 	#print "Fits files: %s" % len(fits_files)
 	for fits_file in fits_files:
 		this_fits = pyfits.open(fits_file)
 		this_header = this_fits[0].header
+		this_data = this_fits[1].data
+		this_num_obs = this_data['JD'].shape[0]
+		num_obs_list.append(this_num_obs)
 		type_list.append(this_header['TYPE_SPEC'])
 
 	print "Fits files: %s" % len(type_list)
 	counter = Counter(type_list)
 
 	print counter
+
+	plt.figure(figsize=(16,8))
+	plt.hist(num_obs_list, bins=100, cumulative=False, histtype='stepfilled')
+	plt.xticks(range(0,1800,100))
+	plt.xlabel('Number of observations')
+	plt.ylabel('Number of lightcurves')
+
+	plt.show()
 
 
 
--- variclass/lstm.py
+++ variclass/lstm.py
@@ -39,23 +39,24 @@ def main():
     parser.add_argument("dir")
     args = parser.parse_args()
 
-    input_dim = 3 # <----  t, dt, q(t-dt)
+    #input_dim = 3 # <----  t, dt, q(t-dt)
+    input_dim = 2 # <----  dt, q(t-dt)
     input_dtype = 'float64'
-    lstm_memory = 32
-    dropout_rate = 0.5
+    #lstm_memory = 32
+    lstm_memory = 64
+    #dropout_rate = 0.5
+    dropout_rate = 0.1
     lambda_loss=5
     #lambda_loss=0.0
-    #batchsize = 32
-    batchsize = 128
-    num_epochs = 500
+    batchsize = 32
+    num_epochs = 1
 
+    """
     type_list = list()
     jd_list = list()
     q_list = list()
     jd_delta_list = list()
     q_pred_list = list()
-
-    max_jd = 0
 
     fits_files = glob.glob(os.path.join(args.dir, '*.fits'))
     for fits_file in fits_files:
@@ -69,33 +70,77 @@ def main():
         this_jd = this_data['JD']
         this_q = this_data['Q']
 
-        if this_jd.shape[0] > max_jd:
-            max_jd = this_jd.shape[0]
-
-        if this_header['TYPE_SPEC'] == 'QSO':
+        if this_header['TYPE_SPEC'].strip() == 'QSO':
             type_list.append(1)
         else:
             type_list.append(0)
-        jd_list.append(this_jd[1:])
-        q_list.append(this_q[:-1])
-        jd_delta_list.append(this_jd[1:] - this_jd[:-1])
-        q_pred_list.append(this_q[1:])
+        #jd_list.append(this_jd[1:])
+        #q_list.append(this_q[:-1])
+        #jd_delta_list.append(this_jd[1:] - this_jd[:-1])
+        #q_pred_list.append(this_q[1:])
+
+        jd_list.append(this_jd)
+        q_list.append(this_q)
+        #jd_delta_list.append(this_jd[1:] - this_jd[:-1])
+        #_pred_list.append(this_q[1:])
+
         this_fits.close()
 
+    np.savez('jd_list', *jd_list)
+    np.savez('q_list', *q_list)
+    np.savez('type_list', *type_list)
+    exit()
+    """
+
+
+    jd_list = []
+    with np.load('jd_list.npz') as jd_npzfile:
+        for _, jd_array in jd_npzfile.iteritems():
+            jd_list.append(jd_array)
+
+    q_list = []
+    with np.load('q_list.npz') as q_npzfile:
+        for _, q_array in q_npzfile.iteritems():
+            q_list.append(q_array)
+
+    class_list = []
+    with np.load('class_list.npz') as class_npzfile:
+        for _, class_array 
+
     # Input data dimensions for matrix
-    max_jd = max_jd-1
+    max_jd = max([x.shape[0] for x in jd_list])
     num_samples = len(jd_list)
 
+    """
     data_X = np.full((num_samples, max_jd, input_dim), 0., dtype=input_dtype)
     for i in xrange(num_samples):
-        data_X[i,:jd_list[i].shape[0],0] = jd_list[i]
-        data_X[i,:jd_delta_list[i].shape[0],1] = jd_delta_list[i]
-        data_X[i,:q_list[i].shape[0],2] = q_list[i]
+        #data_X[i,:jd_list[i].shape[0],0] = jd_list[i]
+        #data_X[i,:jd_delta_list[i].shape[0],1] = jd_delta_list[i]
+        #data_X[i,:q_list[i].shape[0],2] = q_list[i]
+        data_X[i,:jd_list[i].shape[0],0] = jd_list
+        data_X[i,:jd_list[i].shape[0],0] = jd_list
+    """
 
+    jd_matrix = np.full((num_samples, max_jd), 0., dtype=inut_dtype)
+    for i, jd_array in enumerate(jd_list):
+        jd_matrix[i,:jd_array.shape[0]] = jd_array
+
+    q_matrix = np.full((num_samples, max_jd), 0., dtype=input_dtype)
+    for i, q_array in enumerate(q_list):
+        q_matrix[i,:q_array.shape[0]] = q_array
+
+    import ipdb;ipdb.set_trace()
+
+    """
     class_real = np.asarray(type_list, dtype=input_dtype)[..., np.newaxis, np.newaxis]
     next_real = np.full((num_samples, max_jd, 1), 0., dtype=input_dtype)
     for i in xrange(num_samples):
         next_real[i,:q_pred_list[i].shape[0],0] = q_pred_list[i]
+
+    """
+
+    class_matrix = np.full((num_samples, max_jd), 0., dtype=input_dtype)
+    for this_class in 
 
 
     stratified_k_fold = StratifiedKFold(n_splits=3, shuffle=True)
@@ -112,10 +157,11 @@ def main():
         dropout = Dropout(dropout_rate)(lstm)
         time_distributed = TimeDistributed(Dense(1), input_shape=(max_jd, lstm_memory))(dropout)
         last_output = LastOutput(dropout)
-        dense = Dense(1, activation="sigmoid")(last_output)
+        dense1 = Dense(32, activation='sigmoid')(last_output)
+        dense2 = Dense(1, activation="sigmoid")(dense1)
         #class_indicator = ClassIndicator(dense)
 
-        model = Model(inputs=[_input], outputs=[dense, time_distributed])
+        model = Model(inputs=[_input], outputs=[dense2, time_distributed])
         #model = Model(inputs=[_input], outputs=[class_indicator, time_distributed])
         plot_model(model, "model.png")
 
@@ -128,16 +174,18 @@ def main():
 
         model.fit(x=[train_X], y=[train_class, train_next], batch_size=batchsize, epochs=num_epochs)
 
-        test_predict = model.predict(scale_dataset(data_X[test_index]))[0]
+        #test_predict = model.predict(scale_dataset(data_X[test_index]))[0]
+        test_predict = model.predict(scale_dataset(train_X))[0]
         test_predict = np.reshape(test_predict, (test_predict.shape[0]))
 
         test_predict = 1.0*(test_predict > 0.5)
 
-        score = recall_score(np.reshape(class_real[test_index].astype('int'), class_real[test_index].shape[0]), test_predict.astype('int'))
+        #rec_score = recall_score(np.reshape(class_real[test_index].astype('int'), class_real[test_index].shape[0]), test_predict.astype('int'))
+        rec_score = recall_score(np.reshape(class_real[train_index].astype('int'), class_real[train_index].shape[0]), test_predict.astype('int'))
 
-        print "RECALL_SCORE:", score
+        print "RECALL_SCORE:", rec_score
 
-        cv_scores.append(score)
+        cv_scores.append(rec_score)
 
     print cv_scores
 
